68d976448fa1f7b4b2f119d87ade548da80c1198
Jon Delano <jd31068@hotmail.com>
2024-11-28 06:57:52 -0500

    Add project files.

---
+++ CopyData/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSourceDB = New System.Windows.Forms.TextBox()
+        Me.btnSelectSourceDB = New System.Windows.Forms.Button()
+        Me.btnSelectDestinationDB = New System.Windows.Forms.Button()
+        Me.txtDestinationDB = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboTableToCopy = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.btnViewLog = New System.Windows.Forms.Button()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Source Database"
+        '
+        'txtSourceDB
+        '
+        Me.txtSourceDB.Location = New System.Drawing.Point(15, 32)
+        Me.txtSourceDB.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtSourceDB.Name = "txtSourceDB"
+        Me.txtSourceDB.Size = New System.Drawing.Size(509, 20)
+        Me.txtSourceDB.TabIndex = 1
+        '
+        'btnSelectSourceDB
+        '
+        Me.btnSelectSourceDB.Location = New System.Drawing.Point(415, 55)
+        Me.btnSelectSourceDB.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnSelectSourceDB.Name = "btnSelectSourceDB"
+        Me.btnSelectSourceDB.Size = New System.Drawing.Size(107, 23)
+        Me.btnSelectSourceDB.TabIndex = 2
+        Me.btnSelectSourceDB.Text = "Select Source DB"
+        Me.btnSelectSourceDB.UseVisualStyleBackColor = True
+        '
+        'btnSelectDestinationDB
+        '
+        Me.btnSelectDestinationDB.Location = New System.Drawing.Point(392, 164)
+        Me.btnSelectDestinationDB.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnSelectDestinationDB.Name = "btnSelectDestinationDB"
+        Me.btnSelectDestinationDB.Size = New System.Drawing.Size(129, 23)
+        Me.btnSelectDestinationDB.TabIndex = 5
+        Me.btnSelectDestinationDB.Text = "Select Destination DB"
+        Me.btnSelectDestinationDB.UseVisualStyleBackColor = True
+        '
+        'txtDestinationDB
+        '
+        Me.txtDestinationDB.Location = New System.Drawing.Point(15, 141)
+        Me.txtDestinationDB.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtDestinationDB.Name = "txtDestinationDB"
+        Me.txtDestinationDB.Size = New System.Drawing.Size(509, 20)
+        Me.txtDestinationDB.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 123)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Destination Database"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 66)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Table to Copy"
+        '
+        'cboTableToCopy
+        '
+        Me.cboTableToCopy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboTableToCopy.FormattingEnabled = True
+        Me.cboTableToCopy.Location = New System.Drawing.Point(97, 64)
+        Me.cboTableToCopy.Margin = New System.Windows.Forms.Padding(2)
+        Me.cboTableToCopy.Name = "cboTableToCopy"
+        Me.cboTableToCopy.Size = New System.Drawing.Size(257, 21)
+        Me.cboTableToCopy.TabIndex = 7
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(452, 235)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(69, 23)
+        Me.btnSave.TabIndex = 8
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(15, 235)
+        Me.btnRun.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(69, 23)
+        Me.btnRun.TabIndex = 9
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'btnViewLog
+        '
+        Me.btnViewLog.Location = New System.Drawing.Point(88, 235)
+        Me.btnViewLog.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnViewLog.Name = "btnViewLog"
+        Me.btnViewLog.Size = New System.Drawing.Size(69, 23)
+        Me.btnViewLog.TabIndex = 10
+        Me.btnViewLog.Text = "View Log"
+        Me.btnViewLog.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(533, 265)
+        Me.Controls.Add(Me.btnViewLog)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cboTableToCopy)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSelectDestinationDB)
+        Me.Controls.Add(Me.txtDestinationDB)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnSelectSourceDB)
+        Me.Controls.Add(Me.txtSourceDB)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Copy Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSourceDB As TextBox
+    Friend WithEvents btnSelectSourceDB As Button
+    Friend WithEvents btnSelectDestinationDB As Button
+    Friend WithEvents txtDestinationDB As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cboTableToCopy As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnRun As Button
+    Friend WithEvents btnViewLog As Button
+    Protected WithEvents ofd As OpenFileDialog
+End Class
---
+++ CopyData/Form1.vb
@@ -0,0 +1,317 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Menu
+
+Public Class frmMain
+    Dim logFile As StreamWriter
+    Dim startupParameter As String
+    Dim configFileName As String
+
+    Private Sub btnSelectSourceDB_Click(sender As Object, e As EventArgs) Handles btnSelectSourceDB.Click
+
+        ' allow the user to select the source database
+        With ofd
+            .Title = "Select Source Access ACCDB"
+            .DefaultExt = "*.accdb"
+
+            If .ShowDialog Then
+                txtSourceDB.Text = .FileName
+                WriteToLog($"Selected source DB of { .FileName} ")
+            Else
+                txtSourceDB.Text = ""
+            End If
+        End With
+
+        FillSourceTableDropDown(txtSourceDB.Text)
+
+    End Sub
+
+    Private Sub FillSourceTableDropDown(sourceDBFileName As String)
+
+        ' open the Source Access database and list the tables that are available
+        Dim sourceDBCon As OleDbConnection
+        Try
+            Dim conStrBuilder As New OleDbConnectionStringBuilder With {
+                .Provider = "Microsoft.ACE.OLEDB.16.0",
+                .DataSource = sourceDBFileName
+            }
+
+            sourceDBCon = New OleDbConnection(conStrBuilder.ConnectionString)
+            sourceDBCon.Open()
+
+            Dim sourceDBTables As DataTable
+            sourceDBTables = sourceDBCon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, New Object() {Nothing, Nothing, Nothing, "TABLE"})
+
+            Dim dtRow As DataRow
+            cboTableToCopy.Items.Clear()
+
+            For Each dtRow In sourceDBTables.Rows
+                cboTableToCopy.Items.Add(dtRow(2))
+            Next
+
+        Catch ex As Exception
+            MsgBox("Could not fill table dropdown " & vbCrLf & ex.Message)
+            WriteToLog("Could not fill table dropdown " & ex.Message)
+
+        End Try
+
+        If Not sourceDBCon Is Nothing Then sourceDBCon.Close()
+
+    End Sub
+
+    Private Sub btnSelectDestinationDB_Click(sender As Object, e As EventArgs) Handles btnSelectDestinationDB.Click
+        ' allow the user to select the destination database
+        With ofd
+            .Title = "Select Destination Access MDB"
+            .DefaultExt = "Old Access | *.mdb"
+
+            If .ShowDialog Then
+                txtDestinationDB.Text = .FileName
+            Else
+                txtDestinationDB.Text = ""
+            End If
+        End With
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        ' save this information to be used later
+        Dim configFile As StreamWriter
+
+        If File.Exists(configFileName) Then
+            ' there is one of these, just delete it
+            File.Delete(configFileName)
+            WriteToLog("Deleted previous configuration")
+        End If
+
+        ' save the settings
+        configFile = New StreamWriter(configFileName)
+        configFile.WriteLine($"SourceDB,{txtSourceDB.Text}")
+        configFile.WriteLine($"SourceTB,{cboTableToCopy.Text}")
+        configFile.WriteLine($"DestinationDB,{txtDestinationDB.Text}")
+        configFile.Close()
+
+        WriteToLog("Saved configuration")
+        WriteToLog($"    Of {txtSourceDB.Text} {cboTableToCopy.Text} {txtDestinationDB.Text}")
+
+    End Sub
+
+    Private Sub WriteToLog(logMessage As String)
+
+        logFile.WriteLine($"{Format(Now, "MM-dd-yy HH:mm:ss")} - {logMessage}")
+    End Sub
+
+    Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        WriteToLog("**************** Closing")
+        logFile.Close()
+
+    End Sub
+
+    Private Sub cboTableToCopy_Click(sender As Object, e As EventArgs) Handles cboTableToCopy.Click
+        WriteToLog($"Clicked Source table {cboTableToCopy.Text} ")
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+        Dim sourceDBFile As String = ""
+        Dim sourceTableName As String = ""
+        Dim destinationDBFile As String = ""
+
+        If startupParameter Is Nothing Then
+            ' this is running from the form
+            sourceDBFile = txtSourceDB.Text
+            sourceTableName = cboTableToCopy.Text
+            destinationDBFile = txtDestinationDB.Text
+            WriteToLog("**************** Running from the form")
+        Else
+            ' started via a command line 
+            LoadConfiguration("CMD", sourceDBFile, sourceTableName, destinationDBFile)
+        End If
+
+        WriteToLog("Opening databases")
+        WriteToLog($"Source: {sourceDBFile} table: {sourceTableName}")
+        WriteToLog($"Destination: {destinationDBFile}")
+
+        Dim sourceDBCon As OleDbConnection
+        Dim destinationDBCon As OleDbConnection
+
+        Try
+            ' open the databases
+            Dim sourceConStr As New OleDbConnectionStringBuilder With {
+                .Provider = "Microsoft.ACE.OLEDB.16.0",
+                .DataSource = sourceDBFile
+            }
+            sourceDBCon = New OleDbConnection(sourceConStr.ConnectionString)
+            sourceDBCon.Open()
+
+            Dim DestinationConStr As New OleDbConnectionStringBuilder With {
+                .Provider = "Microsoft.ACE.OLEDB.12.0",
+                .DataSource = destinationDBFile
+            }
+            destinationDBCon = New OleDbConnection(DestinationConStr.ConnectionString)
+            destinationDBCon.Open()
+
+        Catch ex As Exception
+
+            WriteToLog("Could not open " & IIf(sourceDBCon Is Nothing, "source", "destination") & " database due to error " & ex.Message)
+
+            If startupParameter Is Nothing Then
+                ' running from the form, display a message box
+                MsgBox(MsgBox("Could not open " & IIf(sourceDBCon Is Nothing, "source", "destination") & " database due to error " & vbCrLf & ex.Message))
+                Exit Sub
+            Else
+                Me.Close()
+            End If
+
+            Exit Sub
+        End Try
+
+        ' export the table to a text file
+        Try
+            WriteToLog($"Exporting Table")
+            File.Delete(Application.StartupPath & "\TableExport.csv")
+            Dim exportSourceTable As New OleDbCommand($"SELECT * INTO [Text;HDR=Yes;DATABASE={Application.StartupPath & "\"}].TableExport.csv FROM {sourceTableName}", sourceDBCon)
+            exportSourceTable.ExecuteNonQuery()
+
+        Catch ex As Exception
+
+            WriteToLog($"Could not export table {sourceTableName}" & ex.Message)
+            If startupParameter Is Nothing Then
+                ' running from the form, display a message box
+                MsgBox($"Could not export table {sourceTableName}" & vbCrLf & ex.Message)
+                Exit Sub
+            Else
+                Me.Close()
+            End If
+
+        End Try
+
+        ' import into the destination MDB
+        WriteToLog("Drop existing table")
+        Dim dropTableCMD As New OleDbCommand($"DROP TABLE {sourceTableName}", destinationDBCon)
+
+        ' the error would be that the table doesn't exist, and that is fine.
+        Try
+            dropTableCMD.ExecuteNonQuery()
+        Catch
+        End Try
+
+        Try
+            WriteToLog($"Importing Table")
+            Dim importTableCMD As New OleDbCommand("SELECT * " &
+            $"INTO {sourceTableName} FROM [Text;FMT=Delimited;HDR=Yes;CharacterSet=850;DATABASE={Application.StartupPath & "\"}].TableExport.csv;", destinationDBCon)
+            importTableCMD.ExecuteNonQuery()
+
+        Catch ex As Exception
+
+            WriteToLog($"Could not import table {sourceTableName}" & ex.Message)
+
+            If startupParameter Is Nothing Then
+                ' running from the form, display a message box
+                MsgBox($"Could not import table {sourceTableName}" & vbCrLf & ex.Message)
+                Exit Sub
+            Else
+                Me.Close()
+            End If
+
+        End Try
+
+        WriteToLog("The process has completed")
+
+        ' if this was executed via command line, then close the form
+        If Not startupParameter Is Nothing Then Me.Close() Else MsgBox("Process complete")
+
+    End Sub
+
+    Private Sub LoadConfiguration(calledFrom As String, Optional ByRef sourceDBFile As String = "", Optional ByRef sourceTableName As String = "", Optional ByRef destinationDBFile As String = "")
+        ' open the configuration file, if running this on the form then use 
+        ' the form controls for the config data, else use the variables
+        ' passed ByRef so the values are passwed back to the calling sub
+        Dim configFile As New StreamReader(configFileName)
+
+        Select Case calledFrom
+            Case "form"
+
+                ' load the saved data
+                WriteToLog("Loading saved data to the form")
+
+                Dim cboValue As String
+                txtSourceDB.Text = Split(configFile.ReadLine, ",")(1)
+
+                ' check to see if the source db is still there before attempting to use it
+                If File.Exists(txtSourceDB.Text) Then
+                    FillSourceTableDropDown(txtSourceDB.Text)
+
+                    cboValue = Split(configFile.ReadLine, ",")(1)
+                    cboTableToCopy.SelectedIndex = cboTableToCopy.Items.IndexOf(cboValue)
+
+                    If cboTableToCopy.SelectedIndex = -1 Then
+                        MsgBox($"The table {cboValue} was not found in the source db")
+                    End If
+                Else
+                    WriteToLog($"Loaded source db {txtSourceDB.Text} doesn't exist.")
+                    MsgBox($"The source db {txtSourceDB.Text} does not exist. Please select another.")
+                    txtSourceDB.Text = ""
+                End If
+
+                txtDestinationDB.Text = Split(configFile.ReadLine, ",")(1)
+                If Not File.Exists(txtDestinationDB.Text) Then
+                    WriteToLog($"Loaded destination db {txtDestinationDB.Text} doesn't exist.")
+                    MsgBox($"The destination db {txtDestinationDB.Text} does not exist. Please select another.")
+                    txtDestinationDB.Text = ""
+                End If
+
+            Case "CMD"
+
+                ' load the values to the variables needed
+                WriteToLog("Loading saved config for command line run")
+                sourceDBFile = Split(configFile.ReadLine, ",")(1)
+                sourceTableName = Split(configFile.ReadLine, ",")(1)
+                destinationDBFile = Split(configFile.ReadLine, ",")(1)
+
+        End Select
+
+        configFile.Close()
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' open the log file
+        Dim logFileName As String = Application.StartupPath & "\CopyData.log"
+        configFileName = Application.StartupPath & "\CopyData.cfg"
+
+        logFile = New StreamWriter(logFileName, True)
+
+        If My.Application.CommandLineArgs.Count > 0 Then
+            WriteToLog("**************** Running from command line")
+            startupParameter = My.Application.CommandLineArgs(0)
+            If startupParameter = "RUN" Then
+                ' the command line to run and then close has been passed
+                If File.Exists(configFileName) Then
+                    btnRun_Click(Nothing, Nothing)
+                Else
+                    WriteToLog($"The configuration file is missing, unable to continue.")
+                    Me.Close()
+                End If
+            Else
+                ' something other than RUN was found in the first parameter
+                WriteToLog($"Invalid argument; {startupParameter} was passed.")
+                Me.Close()
+            End If
+        Else
+            If File.Exists(configFileName) Then
+                ' there is a config file available, load the values
+                LoadConfiguration("form")
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnViewLog_Click(sender As Object, e As EventArgs) Handles btnViewLog.Click
+        ' open notepad with the log file
+        Shell($"notepad.exe {Application.StartupPath & "\CopyData.log"}")
+    End Sub
+End Class
